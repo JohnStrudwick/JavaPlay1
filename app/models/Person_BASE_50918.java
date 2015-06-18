bf2e0622630eada652190e7337d4e96821c388d5
John Strudwick <johnstrudwick@gds3525.local>
2015-06-18 16:24:39 +0100

    added backup files

---
+++ app/models/Person_BASE_50918.java
@@ -0,0 +1,24 @@
+package models;
+
+import com.avaje.ebean.Model;
+import com.sun.javafx.beans.IDProperty;
+import play.data.Form;
+import play.mvc.Result;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+
+/**
+ * Created by johnstrudwick on 09/06/15.
+ */
+@Entity
+public class Person extends Model{
+
+
+    @Id
+    public String id;
+
+    public String name;
+
+}
